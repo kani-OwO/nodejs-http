4ffb99512de1b4af1edbd43047d95e5b8445ef09
kani-OwO <haxa7906@yahoo.co.jp>
2026-04-29 13:46:33 +0900

    / や /enquetes にアクセスした時の処理を追加

---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict'
+const http = require('node:http');
+const pug = require('pug');
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info(
+      `[${now}] 次のIPアドレスからリクエストがありました ${req.socket.remoteAddress}`
+    );
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+    
+    switch (req.method) {
+      case 'GET':
+        if (req.url === '/') {
+          res.write(pug.renderFile('./top.pug'));
+        } else if (req.url === '/enquetes') {
+          res.write(pug.renderFile('./enquetes.pug'));
+        } else if (req.url === '/enquetes/yaki-tofu') {
+          res.write(
+            pug.renderFile('./form.pug', {
+              path: req.url,
+              firstItem: '焼肉',
+              secondItem: '湯豆腐'
+            })
+          );
+        } else if (req.url === '/enquetes/rice-bread') {
+          res.write(
+            pug.renderFile('./form.pug', {
+              path: req.url,
+              firstItem: 'ご飯',
+              secondItem: 'パン'
+            })
+          );
+        } else if (req.url === '/enquetes/sushi-pizza') {
+          res.write(
+            pug.renderFile('./form.pug', {
+              path: req.url,
+              firstItem: '寿司',
+              secondItem: 'ピザ'
+            })
+          );
+        }
+        res.end();
+        break;
+      case 'POST':
+        let rawData = '';
+        req
+          .on('data', chunk => {
+            rawData += chunk;
+          })
+          .on('end', () => {
+            const answer = new URLSearchParams(rawData);
+            const body = `${answer.get('name')}さんは${answer.get('favorite')}に投票しました`;
+            console.info(`[${now}] 投稿: ${body}`);
+            res.write(`<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`);
+            res.end();
+          });
+        break;
+      default:
+        break;
+    }
+  })
+  .on('error', e => {
+    console.error(`[${new Date()}] Server Error`, e);
+  })
+  .on('clientError', e => {
+    console.error(`[${new Date}] Client Error`, e)
+  });
+const port = 8000;
+server.listen(port, () => {
+  console.log(`ポート${port}番 でサーバーが動いてます。`)
+})
